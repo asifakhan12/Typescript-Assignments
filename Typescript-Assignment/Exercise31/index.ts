b70a84ba08c089e981f18f4498109bc82a75ad43
Asifa khan <143596109+asifakhan12@users.noreply.github.com>
2024-05-15 21:05:28 +0500

    typescript assignment

--- Typescript-Assignment/Exercise31/index.ts
+++ Typescript-Assignment/Exercise31/index.ts
@@ -1,4 +1,3 @@
-
 /* No Users: Add an if test to Exercise 30 to make sure the list of users is not empty.
 • If the list is empty, print the message We need to find some users!
 
@@ -6,24 +5,10 @@
 */
 // Non-empty array case
 let userNames = ['admin', 'Ali ahmed', 'Kamran khan', 'Muhammad', 'Ali'];
-//functional case
-function greetUsers(usernames:string[]) {
-    if (usernames.length === 0) {
-        console.log('We need to find some users!');
-        return;
-    }
-    for (const username of usernames) {
-        if (username.toLowerCase() === 'admin') {
-            console.log('Hello admin, would you like to see a status report?');
-        }
-        else {
-            console.log(`Hello ${username}, thank you for logging in again.`);
-        }
-    }
-}
-console.log(`For non empty username:\n`);
-greetUsers(userNames);
-// to make array empty we just assing an empty array to userNames
-userNames = [];
-console.log(`\n\n For empty username:\n`);
-greetUsers(userNames);
+
+if (userNames.length === 0) {
+    console.log('We need to find some users!');
+}else {
+    userNames = [];
+    console.log(`All user has been removed ${userNames.length}`)
+};
